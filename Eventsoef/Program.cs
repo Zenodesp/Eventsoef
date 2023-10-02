f5dd217d3055ac501161039a0bbacfbd22186c58
Zenodesp <Zeno.despiegelaere@student.ehb.be>
2023-10-02 15:56:58 +0200

    Add project files.

---
+++ Eventsoef/Program.cs
@@ -0,0 +1,51 @@
+﻿using Eventsoef;
+
+delegate void EventHandler(string message);
+
+    internal class Program
+    {
+        public static void message(string message)
+        {
+            Console.WriteLine("program reports " + message + ".");
+        }
+        private static void Main(string[] args)
+        {
+           
+            Stacky<int> stacky = new Stacky<int>();
+
+             stacky.stackChange += new EventHandler(message);
+
+            char keuze = '0';
+
+        do
+        {
+            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("Add object to the stack: 1");
+            Console.WriteLine("Remove latest object from the stack: 2");
+            Console.WriteLine("Clear the stack: 3");
+            Console.WriteLine("Show the full stack: 4");
+            keuze = Console.ReadLine()[0];
+            Console.WriteLine();
+
+            switch (keuze)
+            {
+                case '0':
+                    break;
+                case '1':
+                    Console.WriteLine("enter the number you want to add to the stack:");
+                    stacky.AddObject(Convert.ToInt32(Console.ReadLine()));
+                    break;
+                case '2':
+                    stacky.RemoveObject();
+                    break;
+                case '3':
+                    stacky.Clear();
+                    break;
+                case '4':
+                    stacky.Show();
+                    break;
+            }
+        } while (keuze != '0');
+           
+        }
+    }
---
+++ Eventsoef/Stapel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eventsoef
+{
+    internal class Stacky<T>
+    {
+        public event EventHandler stackChange;
+
+        void ifStackChange(string message)
+        {
+            if (stackChange != null)
+            {
+                stackChange(message);
+            }
+        }
+
+        List<T> stack = new List<T>();
+
+        public void AddObject(T addthis)
+        {
+            stack.Add(addthis);
+            ifStackChange("the following object has been added: " + addthis.ToString());
+        }
+
+        public void RemoveObject()
+        {
+            T place = stack[stack.Count - 1];
+            stack.RemoveAt(stack.Count - 1);
+            ifStackChange("the following object has been removed: " + place.ToString());
+        }
+
+        public void Clear()
+        {
+            stack.Clear();
+            ifStackChange("the stack has been cleared");
+        }
+
+        public string Show()
+        {
+            return stack.ToString();
+        }
+        public override string ToString()
+        {
+            string content = "";
+            foreach (T t in stack)
+                content += t.ToString() + "; ";
+            return content;
+        }
+    }
+}
